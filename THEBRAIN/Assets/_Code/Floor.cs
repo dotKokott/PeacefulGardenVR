a0a81699a4752a0d48b916d65b466989b61a0e19
Christian Kokott <christian.kokott@googlemail.com>
2018-08-08 12:20:48 +0200

    tracki tracker floor collision

---
+++ THEBRAIN/Assets/_Code/Floor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Tracker") {
+            Debug.Log("Tracker entered");
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Tracker") {
+            Debug.Log("Tracker Exit");
+        }
+    }
+}
