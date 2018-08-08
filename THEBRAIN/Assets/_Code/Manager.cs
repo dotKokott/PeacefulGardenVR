66a18edaacafbbb1641df853667db4e45b30dac8
Christian Kokott <christian.kokott@googlemail.com>
2018-08-08 11:56:17 +0200

    Added floor manager

--- THEBRAIN/Assets/_Code/Manager.cs
+++ THEBRAIN/Assets/_Code/Manager.cs
@@ -8,8 +8,18 @@ public class Manager : MonoBehaviour {
 
 	public float RenderScale = 1f;
 
+    public float FloorY = 0.4f;
+
+    public Transform Floor;
+
+    public GameObject[] Controllers;
+    public GameObject[] Trackers;
+
 	void Start () {
 	    XRSettings.eyeTextureResolutionScale = RenderScale;	
+        
+        Debug.Log("Setting Floor position");
+        Floor.position = new Vector3(Floor.position.x, FloorY, Floor.position.z);
 	}
 	
 	// Update is called once per frame
