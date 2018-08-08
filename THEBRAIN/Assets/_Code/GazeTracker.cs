246775d9d91960384dd74fb2f094e815cfd7e29d
Christian Kokott <christian.kokott@googlemail.com>
2018-08-08 11:29:14 +0200

    wednesday version

---
+++ THEBRAIN/Assets/_Code/GazeTracker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeTracker : MonoBehaviour {
+
+	private Transform hmd;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        assignHMD();
+		
+        var ray = new Ray(hmd.transform.position, hmd.transform.forward);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, float.MaxValue)) {
+            if(hit.collider.gameObject == this.gameObject) {
+                Grow();
+            }
+        }
+	}
+
+    void Grow() {
+        transform.localScale += Vector3.up * Time.deltaTime;
+    }
+
+    void assignHMD() {
+        if(hmd != null) return;
+
+        SteamVR_TrackedObject[] trackedObjects = FindObjectsOfType<SteamVR_TrackedObject>();
+        foreach (SteamVR_TrackedObject tracked in trackedObjects)
+        {
+            if (tracked.index == SteamVR_TrackedObject.EIndex.Hmd)
+            {
+                hmd = tracked.transform;
+                return;
+            }
+        }
+
+        //Fallback
+        hmd = Camera.main.transform;
+    }
+}
---
+++ THEBRAIN/Assets/_Code/Seed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SteamVR_GazeTracker))]
+public class Seed : MonoBehaviour {
+
+    private SteamVR_GazeTracker tracker;
+
+	void Start () {
+	    tracker = GetComponent<SteamVR_GazeTracker>();
+	}
+		
+	void Update () {    
+        if(tracker.isInGaze) {            
+            //transform.localScale += Vector3.up * Time.deltaTime * 5f;
+        }
+	}
+}
