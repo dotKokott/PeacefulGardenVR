3371c428d397afc53decf68b197c8c1ee9b0aa1d
Christian Kokott <christian.kokott@googlemail.com>
2018-08-08 17:55:58 +0200

    audio + track timeout

--- THEBRAIN/Assets/_Code/TrackSeeder.cs
+++ THEBRAIN/Assets/_Code/TrackSeeder.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrackSeeder : MonoBehaviour {
+    
 
-	// Use this for initialization
+    private float timeoutTime = 0.3f;
+
+    private Collider col;
+
 	void Start () {
-		
+        col = GetComponent<Collider>();		
 	}
 	
+    public void Timeout() {        
+        StartCoroutine(timeout());
+    }
+
+    private IEnumerator timeout() {
+        if(!col.enabled) yield break;
+
+        Debug.Log("tracker off: " + gameObject.name);
+
+        col.enabled = false;
+        yield return new WaitForSeconds(timeoutTime);
+        col.enabled = true;
+
+        Debug.Log("tracker on: " + gameObject.name);
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
